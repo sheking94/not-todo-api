c6859011d4300d56ed4a1ea8748e411efebd9a25
SheKing <oofricoo@gmail.com>
2022-06-02 19:41:51 +0200

    Remove unnecessary verification

--- src/schema/auth.schema.ts
+++ src/schema/auth.schema.ts
@@ -4,10 +4,10 @@ export const createSessionSchema = object({
   body: object({
     email: string({
       required_error: "Email is required.",
-    }).email("Invalid email or password."),
+    }),
     password: string({
       required_error: "Password is required.",
-    }).min(6, "Invalid email or password."),
+    }),
   }),
 });
 
