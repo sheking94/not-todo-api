66e50b4f675c9caf9d036047e4f19fe7c0aa936e
SheKing <oofricoo@gmail.com>
2022-08-11 19:34:33 +0200

    Add update ToDo schema

--- src/schema/todo.schema.ts
+++ src/schema/todo.schema.ts
@@ -1,4 +1,4 @@
-import { object, string, TypeOf } from "zod";
+import { boolean, object, string, TypeOf } from "zod";
 
 export const createToDoSchema = object({
   body: object({
@@ -8,4 +8,16 @@ export const createToDoSchema = object({
   }),
 });
 
+export const updateToDoSchema = object({
+  body: object({
+    description: string({
+      required_error: "ToDo description is required.",
+    }),
+    done: boolean({
+      required_error: "ToDo done property is required.",
+    }),
+  }),
+});
+
 export type CreateToDoInput = TypeOf<typeof createToDoSchema>["body"];
+export type UpdateToDoInput = TypeOf<typeof updateToDoSchema>["body"];
