2d83d349e15ef9589c9e3c3d0c383c1f8a03fad7
SheKing <oofricoo@gmail.com>
2022-08-11 19:34:52 +0200

    Add update ToDo controller

--- src/controller/todo.controller.ts
+++ src/controller/todo.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { omit } from "lodash";
 
-import { CreateToDoInput } from "../schema/todo.schema";
-import { createToDo } from "../service/todo.service";
+import { CreateToDoInput, UpdateToDoInput } from "../schema/todo.schema";
+import { createToDo, updateToDo } from "../service/todo.service";
 
 export async function createToDoHandler(
   req: Request<{}, {}, CreateToDoInput>,
@@ -17,6 +17,27 @@ export async function createToDoHandler(
 
     return res.status(StatusCodes.CREATED).send({
       message: "ToDo created successfully.",
+      todo: omit(todo.toJSON(), ["__v"]),
+    });
+  } catch (e: any) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(e);
+  }
+}
+
+export async function updateToDoHandler(
+  req: Request<{ todoid: string }, {}, UpdateToDoInput>,
+  res: Response
+) {
+  const toDoId = req.params.todoid;
+  const description = req.body.description;
+  const done = req.body.done;
+  const userId = res.locals.user._id;
+
+  try {
+    const todo = await updateToDo({ userId, toDoId, description, done });
+
+    return res.status(StatusCodes.OK).send({
+      message: "ToDo updated successfully.",
       todo: omit(todo.toJSON(), ["__v"]),
     });
   } catch (e: any) {
